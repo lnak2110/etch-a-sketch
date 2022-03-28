ea44ac61f25f9b7f53c3d8c420f6229529eb7e46
akhang <lnak211099@gmail.com>
2022-03-28 16:18:23 +0700

    Set up the grid container and squares

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ drawContainer.classList.add('draw-container');
 container.appendChild(drawContainer);
 
 let nGridSquares = 16;
-
-for ( let i = 1; i <= nGridSquares; i++) {
-    for (let j = 1; j <= i; j++){
-        let gridSquares = document.createElement('div');
+for (let i = 1; i <= nGridSquares; i++) {
+    for (let j = 1; j <= nGridSquares; j++){
+        gridSquares = document.createElement('div');
         gridSquares.classList.add('grid-squares');
         drawContainer.appendChild(gridSquares);
     }
 }
+
+drawContainer.style.gridTemplateColumns = 'repeat(' + nGridSquares + ', 1fr)';
