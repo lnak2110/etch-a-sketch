23875e22d7756ae4f5226b6eef8f64f0243c70e5
akhang <lnak211099@gmail.com>
2022-03-28 13:45:53 +0700

    Append drawContainer to container, add 's' to gridSquare and grid-square

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const container = document.querySelector('#container');
 
 const drawContainer = document.createElement('div');
 drawContainer.classList.add('draw-container');
+container.appendChild(drawContainer);
 
 let nGridSquares = 16;
 
 for ( let i = 1; i <= nGridSquares; i++) {
     for (let j = 1; j <= i; j++){
-        let gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-square');
-        drawContainer.appendChild(gridSquare);
+        let gridSquares = document.createElement('div');
+        gridSquares.classList.add('grid-squares');
+        drawContainer.appendChild(gridSquares);
     }
 }
