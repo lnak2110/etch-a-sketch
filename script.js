16f68aac16af5853bd8b9eea90b1e1af8cea0c6d
akhang <lnak211099@gmail.com>
2022-03-28 17:02:41 +0700

    Make grid div changes color with mouseover

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ drawContainer.classList.add('draw-container');
 container.appendChild(drawContainer);
 
 let nGridSquares = 16;
+
 for (let i = 1; i <= nGridSquares; i++) {
     for (let j = 1; j <= nGridSquares; j++){
-        gridSquares = document.createElement('div');
+        const gridSquares = document.createElement('div');
         gridSquares.classList.add('grid-squares');
         drawContainer.appendChild(gridSquares);
     }
 }
 
+let color = 'blue';
+
 drawContainer.style.gridTemplateColumns = 'repeat(' + nGridSquares + ', 1fr)';
+
+const gridSquares = document.querySelectorAll('.grid-squares');
+gridSquares.forEach((gridSquare) => {
+    gridSquare.addEventListener('mouseover', function (e) {
+        e.target.style.backgroundColor = color;
+    });
+});
+    
