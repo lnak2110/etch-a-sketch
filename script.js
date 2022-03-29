0837956be2a22041e90be5d0b541413b21366828
akhang <lnak211099@gmail.com>
2022-03-29 14:07:35 +0700

    Add a select button with 3 size options

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
 const container = document.querySelector('#container');
-
+const size = document.getElementById('size');
 const drawContainer = document.createElement('div');
 drawContainer.classList.add('draw-container');
 container.appendChild(drawContainer);
 
 let nGridSquares = 16;
 
-for (let i = 1; i <= nGridSquares; i++) {
-    for (let j = 1; j <= nGridSquares; j++){
-        const gridSquares = document.createElement('div');
-        gridSquares.classList.add('grid-squares');
-        drawContainer.appendChild(gridSquares);
-    }
-}
-
 let color = 'blue';
 
-// Make div squares appear as a grid 
-drawContainer.style.gridTemplateColumns = 'repeat(' + nGridSquares + ', 1fr)';
+createGrid(nGridSquares);
 
-const gridSquares = document.querySelectorAll('.grid-squares');
-gridSquares.forEach((gridSquare) => {
-    gridSquare.addEventListener('mouseover', paintGrid);
-});
 
-function paintGrid(e) {
-    e.target.style.backgroundColor = color;
+function createGrid(nGridSquares) {
+    for (let i = 1; i <= (nGridSquares * nGridSquares); i++) {
+        const gridSquares = document.createElement('div');
+        drawContainer.style.gridTemplateColumns = `repeat(${nGridSquares}, 1fr)`;
+        drawContainer.style.gridTemplateRows = `repeat(${nGridSquares}, 1fr)`;
+        drawContainer.appendChild(gridSquares).classList.add('grid-squares');
+    }    
 }
 
-function clearGrid(e) {
+const gridSquares = document.querySelectorAll('.grid-squares');
+function paintGrid() {
+    const gridSquares = document.querySelectorAll('.grid-squares');
     gridSquares.forEach((gridSquare) => {
-        gridSquare.style.backgroundColor = 'white';
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.backgroundColor = color;
+        });
     });
 }
 
+paintGrid();
+
+function clearGrid() {
+    const gridSquares = document.querySelectorAll('.grid-squares');
+    gridSquares.forEach((gridSquare) => {
+        gridSquare.remove();
+    });
+    createGrid(size.value);
+    paintGrid();
+}
+
+function resize() {
+    size.addEventListener('change', (e) => {
+        clearGrid();
+        createGrid(e.target.value);
+        paintGrid();
+    });
+}
+
+resize();
+
 const clear = document.createElement('button');
-clear.textContent = "Clear";
+clear.textContent = 'Clear';
+clear.addEventListener('click', clearGrid);
 container.appendChild(clear);
 
-clear.addEventListener('click', clearGrid);
+
+
